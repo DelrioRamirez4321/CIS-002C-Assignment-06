b1982d86bee4483af56c3a6b90299c7986949f5c
DelrioRamirez4321 <delrioramirez7813@gmail.com>
2020-02-22 20:31:57 -0800

    worked on reliableMultiply function

--- script.js
+++ script.js
@@ -1,34 +1,33 @@
 // 1. Say you have a function, primitiveMultiply, that in 20% of cases multiplies two
 // numbers and in the other 80% of cases raises an exception of type
-// MultiplicatorUnitFailure. Write a function that wraps this clunky function and just
+// MultiplicationUnitFailure. Write a function that wraps this clunky function and just
 // keeps trying until a call succeeds, after which it returns the result. Make sure you
 // handle only the exceptions you are trying to handle.
 // Example Output:
 // console.log(reliableMultiply(8, 8)); // outputs 64
 "use strict";
 
-class MultiplicatorUnitFailure extends Error {}
+class MultiplicationUnitFailure extends Error {}
 
 function primitiveMultiply(a, b) {
   if (Math.random() < 0.2) {
     return a * b;
   } else {
-    throw new MultiplicatorUnitFailure("Klunk");
+    throw new MultiplicationUnitFailure("Clunk");
   }
 };
-
 while (true) {
-
   try {
     let result = primitiveMultiply();
     break;
-  }catch (error) {
-    console.log(error);
+  } catch (error) {
+    console.log(MultiplicationUnitFailure);
   }
-
-}
+};
+console.log(primitiveMultiply(5,10));
 
 
 function reliableMultiply(a, b) {
-  // Your code here.
+  return a * b ;
 };
+console.log(reliableMultiply(8,8));
